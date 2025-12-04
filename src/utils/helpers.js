8e6976cc9dd2bc66ede4f128b359baeec9ad5ff2
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-04 22:02:56 +0100

    Delete src/utils directory

--- src/utils/helpers.js
+++
@@ -1,5 +0,0 @@
-// src/utils/helpers.js
-export const shortenAddress = (address) => {
-  if (!address) return 'No Wallet Connected';
-  return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
-};
