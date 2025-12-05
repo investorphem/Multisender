5c56689c29773d5d68ce856617958a55f3ff7782
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-05 14:35:03 +0100

    Create src/constants/erc20abi.js

---
+++ src/constants/erc20abi.js
@@ -0,0 +1,33 @@
+// src/constants/erc20abi.js
+export const ERC20_ABI = [
+  // balanceOf function
+  {
+    constant: true,
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function",
+  },
+  // approve function
+  {
+    constant: false,
+    inputs: [
+      { name: "_spender", type: "address" },
+      { name: "_value", type: "uint256" },
+    ],
+    name: "approve",
+    outputs: [{ name: "success", type: "bool" }],
+    type: "function",
+  },
+  // Optional: Standard 'Transfer' event ABI for a complete implementation
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: true, name: "from", type: "address" },
+      { indexed: true, name: "to", type: "address" },
+      { indexed: false, name: "value", type: "uint256" },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+];
