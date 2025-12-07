522a031bffb68609123c00cb23cf94b6f17b1717
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 19:19:46 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import { WalletProviders } from './providers';
 
 export const metadata = {
   title: 'Multi-Sender DAp
-  description: 'Send batch tokelly e Chain',
+  description: 'Send batch tokelly Chain',
 export default function RootLayout({child }) {
   return
     <html lang="en">
