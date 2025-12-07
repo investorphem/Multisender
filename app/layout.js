d64474e08b704b24281406d9f88c2ae144302f7c
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 19:17:15 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,7 @@ import { WalletProviders } from './providers';
 export const metadata = {
   title: 'Multi-Sender DApp',
   description: 'Send batch tokens easily on Base Chain',
-};
-
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
