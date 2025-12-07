53b79d38fb9dbffd7757efbf93732559a871861e
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 19:24:06 +0100

    Update providers.jsx

--- app/providers.jsx
+++ app/providers.jsx
@@ -8,7 +8,7 @@ import { base, mainnet } from 'wagmi/chains'
 import { createWeb3Modal } from '@web3modal/wagmi'
 
 // --- Configuration Setup ---
-const projectId  process.env.NEXT_PUBLIC_WALLET_CONNECT_ROJECT_ID; 
+const projectId  process.env.NEXT_PUBLIC_WALLET_CONNECROJECT_ID; 
 
 if (!projectId) {
     console.error('NEXT_PUBLIC_WALLET_CNNECT_PROJECT_ID is not defined.');
