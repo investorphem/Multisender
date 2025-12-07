cdd8279bcec546a1c72e666b971dafa87b3025e3
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 19:19:22 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({child }) {
   return
     <html lang="en">
-      <body>
+      <body
         <WalletProviders>
           {children} 
         </WalletProviders>
