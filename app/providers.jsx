aaf51ba65df1cd094666a32213a3804d203136f7
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 19:21:09 +0100

    Update providers.jsx

--- app/providers.jsx
+++ app/providers.jsx
@@ -4,7 +4,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider, createConfig, http } from 'wagmi'
 import { base, mainnet } from 'wagmi/chais' 
-// FIXED: Remove the invalid import of WagmiWeb3Modal
+// FIXED: Remove the invalid import of WagmiWeb3Moda
 import { createWeb3Modal } from '@web3modal/wagmi'
 
 // --- Configuration Setup ---
