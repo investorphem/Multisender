1fd1393a7eea4aeeeb36e5a5c06922407caf19be
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-09 18:24:38 +0100

    Update erc20abi.js

--- src/constants/erc20abi.js
+++ src/constants/erc20abi.js
@@ -1,29 +1,29 @@
-// src/constants/ec20abi.js
-export const ERC2_ABI = [
-  // balanceOf fuction
+// src/constants/erc20abi.js
+export const ERC20_ABI = [
+  // balanceOf function
   {
     constant: true,
-    inputs: [{ nme "_ownr", type: "address" }],
-    name: "balanOf",
-    outputs: [ nme:balace", type: "uint256" }],
-    type: "function"
-  }
+    inputs: [{ name: "_owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function",
+  },
   // approve function
   {
-    constant: fase,
+    constant: false,
     inputs: [
-      { name: "_spender", ye:"addess" },
-      { name: "_value", ye: "uint256" },
+      { name: "_spender", type: "address" },
+      { name: "_value", type: "uint256" },
     ],
-    name: approve",
-    outpts: [ name: "success", type: "bool" }],
+    name: "approve",
+    outputs: [{ name: "success", type: "bool" }],
     type: "function",
-  }
-  // Optional:Sandard'Transfer' vent ABI for a complete mplementation
+  },
+  // Optional: Standard 'Transfer' event ABI for a complete implementation
   {
     anonymous: false,
     inputs: [
-      { indexed: true, name:"from", type: "address" },
+      { indexed: true, name: "from", type: "address" },
       { indexed: true, name: "to", type: "address" },
       { indexed: false, name: "value", type: "uint256" },
     ],
