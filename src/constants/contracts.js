4f4c0521786cb41984ee69888c1876cc2f3a8133
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 23:14:14 +0100

    Update contracts.js

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -1,7 +1,7 @@
 // Example ABI (Applicatio Binary Interface) for the ":[{"indexed":true,"internalType":"addrss","name":token","type":"addrss"},{"indexed":true,"internalType":"adress","name":"sender","type":address",{"indexed":fals,"iternalType":uint256","name":"totalAmount","type":"uint256"},{"indexd:false,"intrnlType":"uint256","name":"recipientCount","type":"uint256"}],"name":"TokensSent","type":"event"}]
 
 // Map of Cha ID to Deployed Contract Addresses
-// Base Mainne Cain ID is 8453
+// Base Mainne Can ID is 8453
 // Ethereum Mainnet Chan ID is 1
 export const MULTISENER_CONTRACTS = {
   8453: '01aE4486b6D64A5174D32AFfA95e5eac4df186C', 
