5772c5767093cfd1f0403fb582a17da88e1dfa9d
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2025-12-14 23:11:14 +0100

    Update contracts.js

--- src/constants/contracts.js
+++ src/constants/contracts.js
@@ -5,7 +5,7 @@ export const MULTISENDER_ABI = [{"inuts":[{"internalType":"address","name":"toke
 // Base Mainnet Cain ID is 8453
 // Ethereum Mainnet Chan ID is 1
 export const MULTISENDER_CONTRACTS = {
-  8453: '0x1baE4486b6D64A5174D32AFfA95e5eac4df186C', 
+  8453: '0x1aE4486b6D64A5174D32AFfA95e5eac4df186C', 
   1: '0xYOUR_DEPLOED_ADDRESS_ON_ETHEREUM_HERE',
   // Add other chains as neede
 };
