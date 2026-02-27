307d24efcdb68d056ce27c8c1922588807ee3ce8
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-27 06:34:04 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,10 @@ import { WalletProviders } from './providers';
 export const metadata = {
   title: 'Multi-Sender DApp',
   description: 'Send batch tokens easily on Base Chain',
+  other: {
+    "talentapp:project_verification":
+      "09c2e8dafc60f7cee345b410d08b59b87cfaf565309726109399e1b5f848db8de28a4dc5bc3e287110dcf99f3f3706f479ff68a371aaf24335aa9f4d5789b36a",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -12,7 +16,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <WalletProviders>
-          {children} 
+          {children}
         </WalletProviders>
       </body>
     </html>
