56520b558e0c273f7edf878006db79520212456b
Oluwafemi <39197206+investorphem@users.noreply.github.com>
2026-02-27 07:49:07 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,31 @@
-// app/layout.js
 import './globals.css';
 import { WalletProviders } from './providers';
 
 export const metadata = {
   title: 'Multi-Sender DApp',
   description: 'Send batch tokens easily on Base Chain',
+  openGraph: {
+    title: 'Multi-Sender DApp',
+    description: 'Send batch tokens easily on Base Chain',
+    url: 'https://multisender-tan.vercel.app', 
+    siteName: 'Multi-Sender',
+    images: [
+      {
+        url: '/og-image.png', // Path to image in your /public folder
+        width: 1200,
+        height: 630,
+        alt: 'Multi-Sender DApp Preview',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Multi-Sender DApp',
+    description: 'Send batch tokens easily on Base Chain',
+    images: ['/og-image.png'], 
+  },
   other: {
     "talentapp:project_verification":
       "09c2e8dafc60f7cee345b410d08b59b87cfaf565309726109399e1b5f848db8de28a4dc5bc3e287110dcf99f3f3706f479ff68a371aaf24335aa9f4d5789b36a",
